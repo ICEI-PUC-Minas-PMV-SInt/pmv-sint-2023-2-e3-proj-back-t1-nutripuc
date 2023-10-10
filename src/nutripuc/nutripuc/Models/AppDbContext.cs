9fca0eda932224cfeef8b7b3542c9b3dc28dcf60
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2023-10-09 23:54:57 -0300

    Database Config + Entity & Migration

---
+++ src/nutripuc/nutripuc/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+
+namespace nutripuc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+
+        public DbSet<User>? Users { get; set; }
+
+
+    }
+}
--- src/nutripuc/nutripuc/Program.cs
+++ src/nutripuc/nutripuc/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using nutripuc.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
